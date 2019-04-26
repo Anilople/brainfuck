78a32c6f388e749429db0e6ff33a53b2aa41fa98
Anilople <Anilople@outlook.com>
2019-04-26 22:27:31 +0800

    add dynamic memory

--- interpreter.h
+++ interpreter.h
@@ -1,19 +1,44 @@
 #pragma once
 #include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void interpreter(struct Node * node)
 {
-    static char buffer[10000];
+    static int bufferLength = 0;
+    static char * buffer = NULL;
     static int index = 0;
     for(; NULL != node; node = node->circleNext) {
+
+        if(index >= bufferLength) {
+            // memory must increase
+            int oldBufferLength = bufferLength;
+            char * oldBuffer = buffer;
+
+            int newBufferLength = (bufferLength + 1) * 2;
+            char * newBuffer = (char *) malloc(newBufferLength * sizeof(char));
+            
+            if(NULL == newBuffer) {
+                printf("no more memory to use!\n");
+                exit(1);
+            } else {
+                memcpy(newBuffer, oldBuffer, oldBufferLength);
+                // update bufferLength and buffer
+                bufferLength = newBufferLength;
+                buffer = newBuffer;
+            }
+        }
+
         if(Circle == node->nodeType) {
             char value = (char) (node->token.value);
             switch (value)
             {
             case '>': // move to right
                 index += 1;
-                /* code */
+                if(index >= bufferLength) {
+
+                }
                 break;
             case '<': // move to left
                 index -= 1;
