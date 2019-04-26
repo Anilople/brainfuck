0b218f6836b41f934d73743ba05c6542f1cf9bfa
Anilople <Anilople@outlook.com>
2019-04-26 19:37:28 +0800

    lexer function move to .h file

--- lexer.c
+++ lexer.c
@@ -2,50 +2,6 @@
 
 #include <stdio.h>
 
-struct Token char2Token(char c, int line, int lineOffset)
-{
-    struct Token token;
-    token.value = (int) (c);
-    token.line = line;
-    token.lineOffset = lineOffset;
-    return token;
-}
-
-void TokenPrint(struct Token token)
-{
-    printf("%d\t%d\t%c\n", token.line, token.lineOffset, (char)(token.value));
-}
-
-void lexer(int (*getNextChar)(void))
-{
-    int currentLine = 0;
-    int lineOffset = 0;
-    for(int c = getNextChar(); EOF != c; c = getNextChar()) {
-        switch (c)
-        {
-        case '\n':
-            currentLine += 1;
-            lineOffset = 0;
-            break;
-        case '>':
-        case '<':
-        case '+':
-        case '-':
-        case '.':
-        case ',':
-        case '[':
-        case ']': {
-            struct Token token = char2Token(c, currentLine, lineOffset);
-            lineOffset += 1;
-            TokenPrint(token);
-            break;
-        }
-        default:
-            break;
-        }
-    }
-}
-
 int main(int argc, char * argv[])
 {
     lexer(getchar);
--- lexer.h
+++ lexer.h
@@ -1,20 +1,57 @@
 #pragma once
 
+#include <stdio.h>
+
 struct Token {
     int value; /* < > + - . , [ ] */
     int line; // which line token in
     int lineOffset; // token's index in this line [0, ...]
 };
 
-struct Token char2Token(char c, int line, int lineOffset);
+struct Token char2Token(char c, int line, int lineOffset)
+{
+    struct Token token;
+    token.value = (int) (c);
+    token.line = line;
+    token.lineOffset = lineOffset;
+    return token;
+}
+
+void TokenPrint(struct Token token)
+{
+    printf("%d\t%d\t%c\n", token.line, token.lineOffset, (char)(token.value));
+}
 
 /*
-    #line lineOffset value
+    character -> token
+    token -> print it
 */
-void TokenPrint(struct Token token);
-
-/*
-    string to tokens
-    and print them
-*/
-void lexer(int (*getNextChar)(void));
+void lexer(int (*getNextChar)(void))
+{
+    int currentLine = 0;
+    int lineOffset = 0;
+    for(int c = getNextChar(); EOF != c; c = getNextChar()) {
+        switch (c)
+        {
+        case '\n':
+            currentLine += 1;
+            lineOffset = 0;
+            break;
+        case '>':
+        case '<':
+        case '+':
+        case '-':
+        case '.':
+        case ',':
+        case '[':
+        case ']': {
+            struct Token token = char2Token(c, currentLine, lineOffset);
+            lineOffset += 1;
+            TokenPrint(token);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
