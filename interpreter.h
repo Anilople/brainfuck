fbb6093a3373160b1639987e57ad486c6cfad6d5
Anilople <Anilople@outlook.com>
2019-04-27 14:15:46 +0800

    use linklist to manage memory

--- interpreter.h
+++ interpreter.h
@@ -4,66 +4,133 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct BufferNode {
+	char value;
+	struct BufferNode * previous;
+	struct BufferNode * next;
+};
+
+// void BufferNodePrint(struct BufferNode * bufferNode)
+// {
+	// printf(
+		// "%c %d\t0x%x\t0x%x\n",
+		// bufferNode->value,
+		// bufferNode->value,
+		// (bufferNode->previous), 
+		// (bufferNode->next)
+	// );
+// }
+
+void BufferNodeInit(struct BufferNode * bufferNode)
+{
+	bufferNode->value = 0;
+	bufferNode->previous = NULL;
+	bufferNode->next = NULL;
+}
+
+char BufferNodeGetValue(struct BufferNode * bufferNode)
+{
+	return bufferNode->value;
+}
+
+void BufferNodeSetValue(struct BufferNode * bufferNode, char value)
+{
+	bufferNode->value = value;
+}
+
+void BufferNodeValueInc(struct BufferNode * bufferNode)
+{
+	bufferNode->value += 1;
+}
+
+void BufferNodeValueDec(struct BufferNode * bufferNode)
+{
+	bufferNode->value -= 1;
+}
+
+struct BufferNode * BufferNodeGetPrevious(struct BufferNode * bufferNode)
+{
+	return bufferNode->previous;
+}
+
+void BufferNodeSetPrevious(struct BufferNode * bufferNode, struct BufferNode * previous)
+{
+	bufferNode->previous = previous; // previous <- bufferNode
+	previous->next = bufferNode; // previous -> bufferNode
+}
+
+struct BufferNode * BufferNodeGetNext(struct BufferNode * bufferNode)
+{
+	return bufferNode->next;
+}
+
+void BufferNodeSetNext(struct BufferNode * bufferNode, struct BufferNode * next)
+{
+	bufferNode->next = next; // bufferNode -> next
+	next->previous = bufferNode; // bufferNode <- next
+}
+
 void interpreter(struct Node * node)
 {
-    static int bufferLength = 0;
-    static char * buffer = NULL;
-    static int index = 0;
+    static struct BufferNode * bufferNode = NULL;
+	if(NULL == bufferNode) {
+		// just run one time
+		bufferNode = (struct BufferNode *) malloc(sizeof(struct BufferNode));
+		BufferNodeInit(bufferNode);
+	}
+	
     for(; NULL != node; node = node->circleNext) {
-
-        if(index >= bufferLength) {
-            // memory must increase
-            int oldBufferLength = bufferLength;
-            char * oldBuffer = buffer;
-
-            int newBufferLength = (bufferLength + 1) * 2;
-            char * newBuffer = (char *) malloc(newBufferLength * sizeof(char));
-            
-            if(NULL == newBuffer) {
-                printf("no more memory to use!\n");
-                exit(1);
-            } else {
-                memcpy(newBuffer, oldBuffer, oldBufferLength);
-                // update bufferLength and buffer
-                bufferLength = newBufferLength;
-                buffer = newBuffer;
-                
-                // free old memory
-                free(oldBuffer);
-            }
-        }
-
+		// BufferNodePrint(bufferNode);
         if(Circle == node->nodeType) {
             char value = (char) (node->token.value);
             switch (value)
             {
             case '>': // move to right
-                index += 1;
-                if(index >= bufferLength) {
-
-                }
+            {
+				if(NULL != BufferNodeGetNext(bufferNode)) {
+					bufferNode = BufferNodeGetNext(bufferNode);
+				} else {
+					struct BufferNode * newNext = (struct BufferNode *) malloc(sizeof(struct BufferNode));
+					BufferNodeInit(newNext);
+					BufferNodeSetNext(bufferNode, newNext);
+					bufferNode = newNext;
+				}
+			}
                 break;
             case '<': // move to left
-                index -= 1;
+            {
+				if(NULL != BufferNodeGetPrevious(bufferNode)) {
+					bufferNode = BufferNodeGetPrevious(bufferNode);
+				} else {
+					struct BufferNode * newPrevious = (struct BufferNode *) malloc(sizeof(struct BufferNode));
+					BufferNodeInit(newPrevious);
+					BufferNodeSetPrevious(bufferNode, newPrevious);
+					bufferNode = newPrevious;
+				}
+			}
                 break;
             case '+': // increment
-                buffer[index] += 1;
+                BufferNodeValueInc(bufferNode);
                 break;
             case '-': // decrement
-                buffer[index] -= 1;
+                BufferNodeValueDec(bufferNode);
                 break;
             case '.': // output byte
-                printf("%c", buffer[index]);
+                printf("%c", BufferNodeGetValue(bufferNode));
                 break;
             case ',': // input byte
-                scanf("%c", &(buffer[index]));
+			{
+				static char c;
+				scanf("%c", &c);
+				BufferNodeSetValue(bufferNode, c);
+			}
                 break;
             default:
                 break;
             }
         } else {
             // content in []
-            while(0 != buffer[index]) {
+            while(0 != BufferNodeGetValue(bufferNode)) {
                 interpreter(node->squareNext);
             }
         }
